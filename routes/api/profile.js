f2bd855bbb40033dfa0b8d3323ac02df18234513
Jeongho Han <jungho1109@gmail.com>
2019-11-24 21:46:31 -0800

    Delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -163,7 +163,7 @@ router.delete("/", auth, async (req, res) => {
   }
 });
 
-// @route   GET     api/profile/experience
+// @route   PUT     api/profile/experience
 // @desc    Add profile experience
 // @access  Private
 router.put(
@@ -220,3 +220,24 @@ router.put(
     }
   }
 );
+
+// @route   DELETE     api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user:req.user.id});
+        
+        // Get remove index
+        const removeIndex = profile.experience.map(item=>item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex,1);
+        await profile.save();
+        res.json(profile);
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+  });
+
