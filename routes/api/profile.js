71c046fd39f6d011643271bb6c7d1be27c55de14
Jeongho Han <jungho1109@gmail.com>
2019-11-24 20:56:52 -0800

    Get all profiles AND Get a profile by user ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -87,8 +87,8 @@ router.post(
     try {
       let profile = await Profile.findOne({ user: req.user.id });
 
-        // Update
-        if (profile) {
+      // Update
+      if (profile) {
         profile = await Profile.findOneAndUpdate(
           { user: req.user.id },
           { $set: profileFields },
@@ -107,3 +107,41 @@ router.post(
     }
   }
 );
+
+// @route   GET     api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   GET     api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+
+    if(!profile) {
+        return res.status(400).json({msg:'Profile not found'});
+    }
+
+    res.json(profile);
+
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind=='ObjectId'){
+        return res.status(400).json({msg:'Profile not found'});
+    }
+    res.status(500).send("Server Error");
+  }
+});
