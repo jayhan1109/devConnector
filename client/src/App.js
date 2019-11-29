00bed9b881081c8f7c654b33ff6d4340d69a5432
Jeongho Han <jungho1109@gmail.com>
2019-11-28 21:05:23 -0800

    Private Route

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,11 @@ import { Route, Switch } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
 import setAuthToken from "./utils/setAuthToken";
 import { store } from ".";
 import { loadUser } from "./reducers/auth";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 if(localStorage.token){
   setAuthToken(localStorage.token);
@@ -28,6 +30,7 @@ const App = () => {
         <Switch>
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
         </Switch>
       </section>
     </Fragment>
