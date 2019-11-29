17050b0f1ead215a315b0a08ee1cf4cc7bc3bf1e
Jeongho Han <jungho1109@gmail.com>
2019-11-29 10:06:49 -0800

    Create Experience & Education

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import { getCurrentProfile } from "../../reducers/profile";
 import Spinner from "../layout/Spinner";
 import {Link} from 'react-router-dom';
 import DashboardActions from "./DashboardActions";
+import Experience from "./Experience";
+import Education from "./Education";
 
 const Dashboard = ({
   getCurrentProfile,
@@ -26,6 +28,8 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardActions/>
+          <Experience experience={profile.experience}/>
+          <Education education={profile.education}/>
         </Fragment>
       ) : (
         <Fragment><p>You have not yet setup a profile, Please add some info</p>
